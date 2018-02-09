24cc50bb7fccda830c073e64123a43ff6f51304b
Lenninlasd <lenninlasd@gmail.com>
2018-02-09 16:19:51 -0500

    wind speed dependency

--- app.js
+++ app.js
@@ -1,5 +1,132 @@
-
 import * as d3 from "d3";
+
+const metar = {
+    barbs: barbs,
+    stations: getStations
+};
+
+export { metar }
+
+function getStations(geojsonUrl, priority = 2) {
+    return new Promise(function(resolve, reject) {
+
+        d3.json(geojsonUrl, (err, data) => {
+            if(err){
+                reject(Error(err));
+            }
+
+            const stations = data.features.filter(f => {
+                return f.properties.prior <= priority && f.properties.wdir;
+            });
+            resolve(stations);
+        });
+    });
+}
+
+function barbs(stations, barbGroup, projection, path) {
+    const Group = barbGroup
+        .selectAll("wind-barbs")
+        .data(stations).enter()
+        .append("g")
+        .attr("class", d =>  `wspd-${d.properties.wspd}`)
+        .attr("transform", d => {
+            return `translate(${projection(d.geometry.coordinates)})
+                scale(0.2) rotate(${d.properties.wdir})`;
+        });
+
+    renderWindBarbs(Group);
+}
+
+function renderLines(wspd = 25){
+    const numberFlags = Math.floor(wspd/50);
+    if(numberFlags >= 1){
+        wspd = wspd - numberFlags * 50;
+    }
+
+    const numberFlippers = Math.floor(wspd/10);
+    const hasHalfFlippers = wspd % 10 >= 5 ? true : false;
+
+    return addLinesToSvg({numberFlippers, hasHalfFlippers, numberFlags });
+}
+
+function renderWindBarbs(container) {
+
+    container.selectAll("line")
+        .data(d => renderLines(d.properties.wspd) )
+        .enter()
+        .append("line")
+        .attr("x1", d => d.x1)
+        .attr("x2", d => d.x2)
+        .attr("y1", d => d.y1)
+        .attr("y2", d => d.y2)
+        .attr("stroke", d => d.stroke)
+        .style("stroke-width", d => d.strokeWidth);
+}
+
+function addLinesToSvg(newProp){
+
+    const prop = {
+            baseLenght: 100,    paddingTop: 0,
+            paddingLeft: 5,     width: 40,
+            numberFlippers: 3,  flipperPadding: 15,
+            numberFlags: 0,     hasHalfFlippers: true
+    };
+    if(newProp){
+        Object.assign(prop, prop, newProp);
+    }
+
+    const data = [];
+    const windBarbBase = createline({ "x1": prop.paddingLeft, "x2": prop.paddingLeft,
+                               	"y1": prop.baseLenght + prop.width + prop.paddingTop,
+                                "y2": prop.width + prop.paddingTop });
+    data.push(windBarbBase);
+
+    let fliPadding = prop.paddingTop;
+
+    if(prop.numberFlags > 0){
+        fliPadding = prop.paddingTop + prop.numberFlags * prop.flipperPadding;
+    }
+
+    for(let i = 0; i < prop.numberFlippers; i++){
+     	let flipper = createline({ "x1": prop.paddingLeft, "x2": prop.paddingLeft + prop.width,
+                                  "y1": prop.width + fliPadding, "y2": fliPadding });
+       	fliPadding = fliPadding + prop.flipperPadding;
+       	data.push(flipper);
+    }
+
+    if(prop.hasHalfFlippers){
+       	fliPadding = prop.numberFlippers === 0 ? prop.paddingTop + prop.flipperPadding : fliPadding;
+     	let flipper = createline({ "x1": prop.paddingLeft, "x2": prop.paddingLeft + prop.width/2,
+                                  "y1": prop.width + fliPadding, "y2": fliPadding + prop.width/2 });
+        data.push(flipper);
+    }
+
+    return data;
+  }
+
+function createline(l){
+      return {
+              "x1": l.x1 || 0, "x2": l.x2 || 0, "y1": l.y1 || 0, "y2": l.y2 || 0,
+              "stroke": l.stroke || "#555555",
+              "strokeWidth": l.strokeWidth || 4
+      }
+}
+
+
+function addPoligonToSvg(container){
+	const flagData = [{ points: "100,5,135,5,100,45", stroke: "#555555", strokeWidth: 4}];
+
+  	container.selectAll("polygon")
+      .data(flagData)
+      .enter()
+      .append("polygon")
+      .attr("points", d => d.points)
+      .attr("stroke", d => d.stroke)
+      .style("fill", d => d.stroke)
+      .style("stroke-width", d => d.strokeWidth);
+
+}
+
 
 
 //
@@ -39,76 +166,3 @@ import * as d3 from "d3";
 // 							.attr("stroke", d => d.stroke)
 // 							.style("stroke-width", d => d.strokeWidth)
 // 							.style("fill", d => d.fill);
-
-
-export default function addLinesToSvg(container, prop = {
-       baseLenght: 100,
-       paddingLeft: 100,
-       paddingTop: 5,
-       flipperPadding: 15,
-       width: 40,
-       numberFlippers: 3,
-       numberFlags: 0,
-       hasHalfFlippers: true
-   }){
-      const data = [];
-      const base = createline({ "x1": prop.paddingLeft, "x2": prop.paddingLeft,
-                               	"y1": prop.baseLenght + prop.width + prop.paddingTop,
-                                "y2": prop.width + prop.paddingTop });
-      data.push(base);
-
-     let fliPadding = prop.paddingTop;
-
-     if(prop.numberFlags > 0){
-        fliPadding = prop.paddingTop + prop.numberFlags * prop.flipperPadding;
-     }
-
-     for(let i = 0; i < prop.numberFlippers; i++){
-
-     	let flipper = createline({ "x1": prop.paddingLeft, "x2": prop.paddingLeft + prop.width,
-                                  "y1": prop.width + fliPadding, "y2": fliPadding });
-       	fliPadding = fliPadding + prop.flipperPadding;
-       	data.push(flipper);
-     }
-
-     if(prop.hasHalfFlippers){
-       	fliPadding = prop.numberFlippers === 0 ? prop.paddingTop + prop.flipperPadding : fliPadding;
-     	let flipper = createline({ "x1": prop.paddingLeft, "x2": prop.paddingLeft + prop.width/2,
-                                  "y1": prop.width + fliPadding, "y2": fliPadding + prop.width/2 });
-        data.push(flipper);
-     }
-
-     container.selectAll("line")
-      .data(data)
-      .enter()
-      .append("line")
-      .attr("x1", d => d.x1)
-      .attr("x2", d => d.x2)
-      .attr("y1", d => d.y1)
-      .attr("y2", d => d.y2)
-      .attr("stroke", d => d.stroke)
-      .style("stroke-width", d => d.strokeWidth);
-  }
-
-function createline(l){
-      return {
-              "x1": l.x1 || 0, "x2": l.x2 || 0, "y1": l.y1 || 0, "y2": l.y2 || 0,
-              "stroke": l.stroke || "#555555",
-              "strokeWidth": l.strokeWidth || 4
-      }
-}
-
-
-function addPoligonToSvg(container){
-	const flagData = [{ points: "100,5,135,5,100,45", stroke: "#555555", strokeWidth: 4}];
-
-  	container.selectAll("polygon")
-      .data(flagData)
-      .enter()
-      .append("polygon")
-      .attr("points", d => d.points)
-      .attr("stroke", d => d.stroke)
-      .style("fill", d => d.stroke)
-      .style("stroke-width", d => d.strokeWidth);
-
-}
--- render.js
+++ render.js
@@ -1,10 +1,10 @@
 import * as d3 from "d3";
-import addLinesToSvg from "./app";
+import { metar } from "./app";
 
 
 const projection = d3.geoAlbers().scale(1000);
 const path = d3.geoPath().projection(projection);
-const priority = 2.5;
+const priority = 2;
 const width = 960;
 const height = 500;
 
@@ -12,35 +12,20 @@ const svgContainer = d3.select("body").append("svg")
                         .attr("width", width)
                         .attr("height", height);
 
+const barbGroup = svgContainer.append("g").attr("class", "wind-barbs");
 
-d3.json("https://geojson-ldxdcwirxw.now.sh/metar-api-sample-json-10945.json", (err, data) => {
-    const station = data.features.filter(f => {
-        return f.properties.prior <= priority && f.properties.wdir;
-    }).map(f => {
-      	return {
-        	"geometry": {
-               "type": "Point",
-               "coordinates": f.geometry.coordinates
-           	},
-          	"properties": f.properties,
-          	"type": f.type
-        }
-    });
-    ready(svgContainer, path, station, projection);
-});
+const geojsonUrl = "https://geojson-ldxdcwirxw.now.sh/metar-api-sample-json-10945.json";
+metar.stations(geojsonUrl, 3).then(stations => {
+    metar.barbs(stations, barbGroup, projection);
+    // ready(svgContainer, path, stations, projection);
+})
 
 function ready(svg, path, data, projection) {
 
-    const Group = svg
-        .selectAll(".wind-barbs")
+    svg.selectAll(".shadows")
         .data(data)
-        .enter()
-        .append("g")
-        .attr("class", d =>  `wspd-${d.properties.wspd}`)
-        .attr("transform", d => {
-              return `translate(${projection(d.geometry.coordinates)})
-        			rotate(${d.properties.wdir}) scale(0.2)`;
-           	});
-
-    addLinesToSvg(Group)
+        .enter().append("path")
+        .attr("class", "point")
+        .attr("d", path)
+        .attr("fill", "pink");
 }
